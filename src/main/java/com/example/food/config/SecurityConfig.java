b7cdf21658c2b6c1c97580981a9ce8a2016e10ff
syLee <tjdduq410@naver.com>
2024-11-22 17:00:47 +0900

    유저 정보 로직 완료

---
+++ src/main/java/com/example/food/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+//package com.example.food.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//
+//    //  비밀번호를 암호화하는 코드.
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
