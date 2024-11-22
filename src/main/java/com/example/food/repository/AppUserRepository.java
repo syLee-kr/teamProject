f33fa923090e3ff2bb1e01032c2c0528a3475908
syLee <tjdduq410@naver.com>
2024-11-22 16:09:03 +0900

    유저 정보 로직 추가

---
+++ src/main/java/com/example/food/repository/AppUserRepository.java
@@ -0,0 +1,22 @@
+package com.example.food.repository;
+
+import com.example.food.domain.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+
+    // 유저 ID를 바탕으로 유저 정보를 가져온다.
+    AppUser findByUsername(String userId);
+
+    // 유저 이메일과 아이디를 바탕으로 유저 정보를 알려준다.
+    AppUser findByEmail(String userId, String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE AppUser m SET m.password = :password WHERE m.username = :username")
+    void changePassword(@Param("username") String username, @Param("password") String password);
+}
