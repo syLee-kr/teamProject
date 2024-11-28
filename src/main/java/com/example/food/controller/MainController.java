605aca0bc9193c7c7e4f77cd3d6fbdc5381fd141
syLee <tjdduq410@naver.com>
2024-11-28 14:26:40 +0900

    객체 수정

--- src/main/java/com/example/food/controller/MainController.java
+++ src/main/java/com/example/food/controller/MainController.java
@@ -2,12 +2,14 @@ package com.example.food.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/main")
 public class MainController {
 
-    @GetMapping("/main")
-    public String main() {
+    @GetMapping
+    public String mainPage() {
         return "main/main";
     }
 }
--- src/main/java/com/example/food/domain/Comment.java
+++ src/main/java/com/example/food/domain/Comment.java
@@ -1,12 +1,15 @@
 package com.example.food.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -21,32 +24,32 @@ import java.time.LocalDateTime;
 public class Comment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int cseq;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "comment_seq_generator")
+    @SequenceGenerator(
+            name = "comment_seq_generator",   // 시퀀스 생성기의 이름
+            sequenceName = "comment_seq",     // 실제 데이터베이스 시퀀스 이름
+            initialValue = 1,                // 시작 값
+            allocationSize = 1               // 증가 크기
+    )
+    private Long cSeq;
 
     @Column(nullable = false)
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
+    @JsonBackReference // 자식 필드
     private Post post;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
-    private AppUser appUser;
+    private User appUser;
 
+    @CreationTimestamp              //  자동으로 시간을 적용
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
+    @Column(updatable = false)
     private LocalDateTime updatedAt;
-
-    @PrePersist
-    public void prePersist() {
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now(); // 생성 시 업데이트 시간도 동일하게 설정
-    }
-
-    @PreUpdate
-    public void preUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
 }
