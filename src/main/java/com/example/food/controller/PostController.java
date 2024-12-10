8132f6aafa6c2e07320073505f4bce690f031443
PSJ <skyrimdata@naver.com>
2024-12-10 16:37:46 +0900

    testPostList > priority sort add

--- src/main/java/com/example/food/controller/PostController.java
+++ src/main/java/com/example/food/controller/PostController.java
@@ -2,6 +2,7 @@ package com.example.food.controller;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,7 @@ public class PostController {
 	public String testPostList(Model model){
 		List<Post> postList= new ArrayList<Post>();
 		
+		
 		//임시 게시물
 		for (int i =0; i <9; i++) {
 			Post post = new Post();
@@ -40,11 +42,20 @@ public class PostController {
 			
 			post.setContent("글내용 " + i);
 			post.setPostdate(LocalDateTime.now());
-			post.setPriority(i);
+			
+			if(i == 0) {
+				post.setPriority(1); // 공지	
+			}else {
+				post.setPriority(0); // 일반
+			}
+			
 			post.setCnt(0L);
 			postList.add(post);
 			
 		}
+		// priority 내림차순 정렬
+		postList.sort(Comparator.comparing(Post::getPriority).reversed());
+		
 		model.addAttribute("postList", postList);
 		return "testPostList";
 	}
