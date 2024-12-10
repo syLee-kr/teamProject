a6b8015df91ca2167a2882ab35f20c55926ab8fe
PSJ <skyrimdata@naver.com>
2024-12-10 17:58:25 +0900

    postcontroller update

--- src/main/java/com/example/food/service/postservice/PostService.java
+++ src/main/java/com/example/food/service/postservice/PostService.java
@@ -1,5 +1,14 @@
 package com.example.food.service.postservice;
 
+import java.util.List;
+
+import com.example.food.domain.Post;
+
 public interface PostService {
+
+	static List<Post> getPostList(Integer pageNum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
