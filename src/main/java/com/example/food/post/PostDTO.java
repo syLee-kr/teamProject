0eb6f185f64708e80d1dd520f5259f4aca6417e1
PSJ <skyrimdata@naver.com>
2024-12-11 12:05:03 +0900

    postServieImpl update

--- src/main/java/com/example/food/post/PostDTO.java
+++ src/main/java/com/example/food/post/PostDTO.java
@@ -1,12 +1,35 @@
 package com.example.food.post;
 
+import java.time.LocalDateTime;
+
+import com.example.food.domain.Post;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@Data
 @NoArgsConstructor
-public class PostDTO {public void setPSeq(Long pSeq) {
-		// TODO Auto-generated method stub
-		
+@AllArgsConstructor
+public class PostDTO {
+	private Long pSeq;
+	private String title;
+	private String content;
+	private LocalDateTime postdate;
+	private int priority;
+	private Long cnt;
+	
+	private String userId;
+	private String Name;
+	
+	public PostDTO(Post post) {
+		this.pSeq = post.getPSeq();
+		this.title = post.getTitle();
+		this.content = post.getContent();
+		this.postdate = post.getPostdate();
+		this.priority = post.getPriority();
+		this.cnt = post.getCnt();
+		this.userId = post.getUserId();
+		this.Name = post.getName();
 	}
-
 }
