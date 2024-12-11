84c177d9743b1ad088ab5dd10171fbeffb34255c
PSJ <skyrimdata@naver.com>
2024-12-11 13:19:20 +0900

    PostDAO del

--- src/main/java/com/example/food/service/postservice/PostService.java
+++ src/main/java/com/example/food/service/postservice/PostService.java
@@ -2,28 +2,36 @@ package com.example.food.service.postservice;
 
 import java.util.List;
 
+import com.example.food.PostDTO;
 import com.example.food.domain.Post;
-import com.example.food.post.PostDTO;
 
 public interface PostService {
-
-	static List<Post> postList(Integer pageNum) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
+	// 게시물 목록
+	List<PostDTO> getPostList(Integer pageNum);
+	
+	// 게시물 업데이트
 	void updatePost(PostDTO postDto);
-
+	
+	// 게시물 삭제
+	
 	void deletePost(Long pSeq);
-
+	
+	// 게시물 저장
 	void savePost(PostDTO postDto);
 
-	List<PostDTO> getPostList(Integer pageNum);
-
+	// 페이지 네이션 리스트
 	Integer[] getPageList(Integer pageNum);
 
+	// 게시물 상세
 	PostDTO getPostById(Long pSeq);
 
+	// 게시물 조회
 	Post getPost(Long pSeq);
+	
+	//test
+	static List<Post> postList(Integer pageNum) { 
+		return null;
+	}
 	
 }
