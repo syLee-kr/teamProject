32ec24577bae07fc3c257296a3baf3dde4445279
PSJ <skyrimdata@naver.com>
2024-12-11 16:41:58 +0900

    post, repository,service update

--- src/main/java/com/example/food/PostDTO.java
+++ src/main/java/com/example/food/PostDTO.java
@@ -18,7 +18,6 @@ public class PostDTO {
 	private LocalDateTime postdate;
 	private int priority;
 	private Long cnt;
-	
 	private String userId;
 	private String Name;
 	
@@ -29,7 +28,8 @@ public class PostDTO {
 		this.postdate = post.getPostdate();
 		this.priority = post.getPriority();
 		this.cnt = post.getCnt();
-		this.userId = post.getUserId();
-		this.Name = post.getName();
+		// post 객체의 user 필드를 통해 userId/name 가져옴
+		this.userId = post.getUser().getUserId();
+		this.Name = post.getUser().getName();
 	}
 }
