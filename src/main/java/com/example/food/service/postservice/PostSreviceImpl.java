210cbeaed6c43a529e2d52be2d4820a95efcf26f
PSJ <skyrimdata@naver.com>
2024-12-11 15:10:47 +0900

    serviceImpl update

--- src/main/java/com/example/food/service/postservice/PostSreviceImpl.java
+++ src/main/java/com/example/food/service/postservice/PostSreviceImpl.java
@@ -3,6 +3,9 @@ package com.example.food.service.postservice;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.food.PostDTO;
@@ -16,11 +19,27 @@ import lombok.AllArgsConstructor;
 public class PostSreviceImpl implements PostService{
 	
 	private final PostRepository postRepository;
-
+	
+	// DTO return
+	@Override 
+	public PostDTO getPostById(Long pSeq) {
+		
+		Post post = postRepository.findById(pSeq).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 존재하지 않습니다."));
+		
+		return new PostDTO(post);
+	}
+	
+	// Entity return
+	@Override 
+	public Post getPost(Long pSeq) {
+		return postRepository.findById(pSeq).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 존재하지 않습니다."));
+	}
+	
+	// Post update
 	@Override
 	public void updatePost(PostDTO postDto) {
 		Post post = postRepository.findById(postDto.getPSeq())
-				.orElseThrow(()-> new IllegalArgumentException("Post not found"));
+				.orElseThrow(()-> new IllegalArgumentException("해당 게시물이 존재하지 않습니다."));
 		post.setTitle(postDto.getTitle());
 		post.setContent(postDto.getContent());
 		post.setPriority(postDto.getPriority());
@@ -28,13 +47,15 @@ public class PostSreviceImpl implements PostService{
 		
 		postRepository.save(post);
 	}
-
+	
+	// Post del
 	@Override
 	public void deletePost(Long pSeq) {
 		postRepository.deleteById(pSeq);
 		
 	}
-
+	
+	// Post save
 	@Override
 	public void savePost(PostDTO postDto) {
 		Post post = new Post();
@@ -45,40 +66,31 @@ public class PostSreviceImpl implements PostService{
 		
 		postRepository.save(post);
 	}
-
+	
+	// Paging process
 	@Override
 	public List<PostDTO> getPostList(Integer pageNum) {
-		int pageSize = 10;
-		List<Post> posts = postRepository.findAll();
-		// 페이지 네이션에 맞게 수정??
+		int pageSize = 10; // 페이지당 게시물 수 10개
+		Pageable pageable = PageRequest.of(pageNum -1, pageSize);
+		Page<Post> postPage = postRepository.findAll(pageable); // 페이지 요청해서 게시물 목록 가저옴
 		
-		return posts.stream().map(post -> new PostDTO(post)).collect(Collectors.toList());
+		return postPage.getContent().stream()						// 페이지에서 게시물 목록 가져와
+									.map(post -> new PostDTO(post)) // post entity를 postDTO로 변환
+									.collect(Collectors.toList());	// List<PostDTO>로 return
 	}
 
 	@Override
 	public Integer[] getPageList(Integer pageNum) {
 		
 		int pageSize = 10;
-		int totalPosts = (int)postRepository.count();
-		int totalPages = (int)Math.ceil((double) totalPosts/pageSize);
+		int totalPosts = (int)postRepository.count(); // 전체게시물
+		int totalPages = (int)Math.ceil((double) totalPosts/pageSize); // 전체 페이지 수(올림처리) 
 		
-		Integer[] pageList = new Integer[totalPages];
-		for (int i =0; i < totalPages; i++) {
-			pageList[i] = i + 1;
+		Integer[] pageList = new Integer[totalPages]; // 페이지 수만큼 배열
+		for (int i =0; i < totalPages; i++) { // 전체 페이지 수만큼 반복
+			pageList[i] = i + 1; // 페이지 번호 1부터 시작
 		}
 		return pageList;
 	}
 
-	@Override
-	public PostDTO getPostById(Long pSeq) {
-		
-		Post post = postRepository.findById(pSeq).orElseThrow(() -> new IllegalArgumentException("Post not found"));
-		
-		return new PostDTO(post);
-	}
-
-	@Override
-	public Post getPost(Long pSeq) {
-		return postRepository.findById(pSeq).orElseThrow(() -> new IllegalArgumentException("Post not found"));
-	}
 }
