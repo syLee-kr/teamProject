9aefb115e90c9abe36ad52f9adc2aee469fdf0c0
PSJ <skyrimdata@naver.com>
2024-12-12 17:55:44 +0900

    TestEntityManager add 수정 필요

--- src/test/java/com/example/food/persistence/PostRepositoryTest.java
+++ src/test/java/com/example/food/persistence/PostRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.food.persistence;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +23,9 @@ public class PostRepositoryTest {
 	@Autowired
 	private UserRepository userRepo;
 	
+	@Autowired
+	private TestEntityManager entityManager;
+	
 	//@Disabled
 	@Test
 	@Transactional   //테스트 후 롤백 > 데이터베이스 저장안됨
@@ -39,7 +43,11 @@ public class PostRepositoryTest {
 		//user.setRole(Users.Role.ROLE_ADMIN); // 명시적으로 ROLE_ADMIN 설정
 		
 		// User 엔티티 먼저 저장
-		Users savedUser = userRepo.save(user);
+		//Users savedUser = userRepo.save(user);
+		
+		//TestEntityManager 적용 User
+		Users savedUser = entityManager.persistAndFlush(user);
+		
 		
 		// Post 객체 생성
 		Post post = new Post();
@@ -51,7 +59,10 @@ public class PostRepositoryTest {
 		post.setUser(savedUser); // 저장된 user를 Post에 설정
 		
 		// Post 엔티티 저장
-		Post savePost = postRepo.save(post);
+		//Post savePost = postRepo.save(post);
+		
+		//TestEntityManager 적용 Post
+		Post savedPost = entityManager.persistAndFlush(post);
 		
 		// 데이터 확인(저장된 데이터가 기대하는 값과 일치하는지 체크) > 롤백으로 인한 데이터확인임
 		assertNotNull(savePost.getPSeq());
