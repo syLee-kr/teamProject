6909dd933c8df16484f7f7367e26c0a655b85360
PSJ <skyrimdata@naver.com>
2024-12-13 11:40:42 +0900

    pageNum Validate add

--- src/test/java/com/example/food/persistence/PostRepositoryTest.java
+++ src/test/java/com/example/food/persistence/PostRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.food.persistence;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,7 +15,9 @@ import com.example.food.repository.UserRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 @SpringBootTest
+@AutoConfigureTestEntityManager
 public class PostRepositoryTest {
 	
 	@Autowired
@@ -42,7 +45,7 @@ public class PostRepositoryTest {
 		user.setRole(Users.Role.ROLE_USER);
 		//user.setRole(Users.Role.ROLE_ADMIN); // 명시적으로 ROLE_ADMIN 설정
 		
-		// User 엔티티 먼저 저장
+		// User 엔티티에 먼저 저장
 		//Users savedUser = userRepo.save(user);
 		
 		//TestEntityManager 적용 User
@@ -58,20 +61,20 @@ public class PostRepositoryTest {
 		post.setCnt(0L);
 		post.setUser(savedUser); // 저장된 user를 Post에 설정
 		
-		// Post 엔티티 저장
+		// Post 엔티티에 저장
 		//Post savePost = postRepo.save(post);
 		
 		//TestEntityManager 적용 Post
 		Post savedPost = entityManager.persistAndFlush(post);
 		
 		// 데이터 확인(저장된 데이터가 기대하는 값과 일치하는지 체크) > 롤백으로 인한 데이터확인임
-		assertNotNull(savePost.getPSeq());
-		assertEquals("테스트 제목", savePost.getTitle());
-		assertEquals("테스트 내용", savePost.getContent());
-		assertEquals(1, savePost.getPriority());
-		assertEquals(0L, savePost.getCnt());
-		assertEquals("test123", savePost.getUser().getUserId());
-		assertEquals("테스트유저", savePost.getUser().getName());
+		assertNotNull(savedPost.getPSeq());
+		assertEquals("테스트 제목", savedPost.getTitle());
+		assertEquals("테스트 내용", savedPost.getContent());
+		assertEquals(1, savedPost.getPriority());
+		assertEquals(0L, savedPost.getCnt());
+		assertEquals("test123", savedPost.getUser().getUserId());
+		assertEquals("테스트유저", savedPost.getUser().getName());
 	
 	}
 }
