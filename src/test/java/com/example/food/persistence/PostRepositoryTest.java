1c21157c2128799e96dd78685ff72eec4386d911
PSJ <skyrimdata@naver.com>
2024-12-17 09:24:45 +0900

    Post/comment code add & not yet test insert data

--- src/test/java/com/example/food/persistence/PostRepositoryTest.java
+++ src/test/java/com/example/food/persistence/PostRepositoryTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.transaction.TestTransaction;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.food.domain.Post;
